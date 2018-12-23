69e52fda6f642745b7a84588441874c367a57f44
Shashikant86 <shashikant.jagtap@icloud.com>
2018-12-23 17:24:40 +0000

    token changes

--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 set -e
 git config --global github.user Shashikant86
-git config remote.origin.url https://Shashikant86:$1@github.com/Shashikant86/SwiftPM-GitHubAction.git
+# git config remote.origin.url https://Shashikant86:$1@github.com/Shashikant86/SwiftPM-GitHubAction.git
 git tag 0.0.1
-git push origin 0.0.1
+git push https://Shashikant86:$1@github.com/Shashikant86/SwiftPM-GitHubAction.git --tags
